30d2532f0e8aef045dbc39a9f33e753b45fe0000
Mike Bourgeous <mike@mikebourgeous.com>
2025-04-04 14:22:20 -0600

    Add SimpleCov to try to reproduce the mb-sound bug.  Still works though.

--- ruby34_bug_helper.rb
+++ ruby34_bug_helper.rb
@@ -2,9 +2,19 @@
 #
 # See https://github.com/mike-bourgeous/mb-sound/issues/36
 
+require 'simplecov'
+
 puts 'This is the helper.  It begins.'
 $helper=true
+
+SimpleCov.start do
+  SimpleCov.command_name "#{$0} #{$$} #{Random.random_number}"
+  SimpleCov.formatter SimpleCov::Formatter::SimpleFormatter
+  SimpleCov.minimum_coverage 0
+end
+
 require File.expand_path($0, '.')
+
 puts 'The helper shall now depart.'
 exit 0
 puts 'The helper has remained beyond its welcome.'
