e15db5b92dcea6bee51f5764ce67cc07d72196c0
Mike Bourgeous <mike@mikebourgeous.com>
2025-04-05 23:39:58 -0600

    Also run the test under Valgrind.

--- spec/bin/midi_roll_spec.rb
+++ spec/bin/midi_roll_spec.rb
@@ -1,7 +1,13 @@
 RSpec.describe('bin/midi_roll.rb', :aggregate_failures) do
   it 'can display a MIDI roll' do
     if ENV['INTERACTIVE_GDB'] == '1'
+      MB::U.headline 'Starting interactive GDB'
       system("gdb -x ./gdb_ruby_backtrace.gdb --args $(which ruby) bin/midi_roll.rb -r 2 -c 100 -n C3 spec/test_data/all_notes.mid 2>&1")
+    end
+
+    if ENV['VALGRIND'] == '1'
+      MB::U.headline 'Starting valgrind'
+      system("valgrind --undef-value-errors=no -s $(which ruby) bin/midi_roll.rb -r 2 -c 100 -n C3 spec/test_data/all_notes.mid 2>&1")
     end
 
     # unbuffer is from the expect package and uses a pseudoterminal to make gdb print colors
